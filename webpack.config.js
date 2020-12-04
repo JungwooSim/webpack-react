5da4b0837c81d56427c38553c36afe460332ae66
JungwooSim <cscd053@gmail.com>
2020-12-04 14:34:20 +0900

    기본 틀, 테스트 완료

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve("dist"),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: 'public/index.html',
+  //   })
+  // ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+  },
+};
