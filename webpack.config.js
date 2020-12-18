fe9d5482e7379916d81613526ce5d75d4adb37ec
JungwooSim <cscd053@gmail.com>
2020-12-18 22:35:38 +0900

    dotenv 플러그인 추가 및 배포시 분기 로직 베이스 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,60 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const port = process.env.PORT || 3000;
 const path = require("path");
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 
-module.exports = {
-  mode: 'development',
-  entry: './react-app/index.js',
-  output: {
-    path: path.resolve("dist"),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-            },
+module.exports = (env) => {
+
+  dotenv.config({
+    path: path.resolve(`react-app/env/${env.file_name || 'real'}.env`)
+  });
+
+  return {
+    mode: 'development',
+    entry: './react-app/index.js',
+    output: {
+      path: path.resolve("dist"),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
-        ],
-      },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'react-app/public/index.html',
+      }),
+
+      // 변수를 전역으로 사용할 수 있도록 해주는 플러그인
+      new webpack.DefinePlugin({
+        'process.env.test' : JSON.stringify(process.env.test)
+      }),
+
+      // 변수 내보내기
+      new webpack.EnvironmentPlugin(['test'])
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'react-app/public/index.html',
-    })
-  ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-  },
+    devServer: {
+      host: 'localhost',
+      port: port,
+    },
+  };
 };
